995f983a6bf7be1b0120f60b7518fd4a9eb8840e
viewthiti <65011212173@msu.ac.th>
2024-10-09 00:16:29 +0700

    img

--- api/register.ts
+++ api/register.ts
@@ -13,23 +13,14 @@ router.get("/Users", (req, res) => {
   });
   
 
-// //สมัคร Users
-// router.post("/", (req,res)=>{
-//     const users: Users = req.body;
-
-//     if (users.password !== users.confirmPassword) {
-//         return res.status(400).json({ error: "Passwords do not match." });
-//     }
-
-
-// });
+//สมัคร Users
 
 router.post("/register", (req, res) => {
     const users: Users = req.body;
 
     if (users.password !== users.confirmPassword) {
-        res.status(400).json({ error: "Passwords do not match." });
-        return 
+       res.status(400).json({ error: "Passwords do not match." });
+       return
     }
 
     let checkphoneSql = "SELECT * FROM `Users` WHERE `phone` = ?";
@@ -85,3 +76,73 @@ router.post("/register", (req, res) => {
     });
 });
 
+
+//Firebase
+// 1.connect filebase
+import { initializeApp } from "firebase/app";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
+// import { lmage } from "../model/Image_get_res";
+import multer from "multer";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKWNIrnLBDZI5hsVsjJXC_U7tTpWsoL6s",
+    authDomain: "miniproject2-cs.firebaseapp.com",
+    projectId: "miniproject2-cs",
+    storageBucket: "miniproject2-cs.appspot.com",
+    messagingSenderId: "29183647488",
+    appId: "1:29183647488:web:23753ae01752da8cd89de5",
+    measurementId: "G-BN4NRHMWNC"
+  };
+
+//เชื่อม firebase
+initializeApp(firebaseConfig);
+const storage = getStorage();
+
+class FileMiddleware {
+  //Attribute filename
+  filename = "";
+
+  //Attribute diskLoader
+  //Create object of diskLoader for saving file
+  public readonly diskLoader = multer({
+    storage: multer.memoryStorage(),
+    //limits file size
+    limits: {
+      fileSize: 67108864, // 64 MByte
+    },
+  });
+}
+
+//POST/Upload
+const fileUpload = new FileMiddleware();
+router.post("/", fileUpload.diskLoader.single("file"), async (req, res) => {
+  // 2.uplaod file to firebase
+  //Genarate filename
+  const filename =
+    Date.now() + "-" + Math.round(Math.random() * 10000) + ".png";
+
+  //define saving filename On firebase
+  const storageRef = ref(storage, "/images/" + filename);
+  //defind detail
+  const metadata = {
+    contentType: req.file!.mimetype,
+  };
+  //upload
+  const snapshot = await uploadBytesResumable(
+    storageRef,
+    req.file!.buffer,
+    metadata
+  );
+
+  const url = await getDownloadURL(snapshot.ref);
+  res.status(200).json({
+    url: url,
+  });
+
+});

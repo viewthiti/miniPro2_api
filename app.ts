f055b8e2258a89a12e8379bc59132727d3a1216d
viewthiti <65011212173@msu.ac.th>
2024-10-10 19:30:15 +0700

    phone search

--- app.ts
+++ app.ts
@@ -19,8 +19,8 @@ app.use(bodyParser.json());
 
 app.use(
     cors({
-      // origin: "*",
-      origin: "http://localhost:4200",
+      origin: "*",
+      // origin: "http://localhost:4200",
     })  
   );
   

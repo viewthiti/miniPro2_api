43a48af682cb625b0925b330b5c272fd8f996da4
viewthiti <65011212173@msu.ac.th>
2024-10-10 18:52:09 +0700

    get user

---
+++ api/users.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { conn, queryAsync } from "../dbconnect";
+import mysql from "mysql";
+// import { editUsers } from "../model/edit_get_res";
+import { log } from "console";
+
+
+export const router = express.Router();
+
+//ดึงข้อมูลมาโชว์
+router.get("/:userID", (req, res) => {
+    const userID = +req.params.userID;
+    const sql = "select userID, phone, name, address, lat, long, image from Users where userID = ?";
+    // log(userID);
+    conn.query(sql, [userID], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+      res.json(result[0]);
+    });
+   
+  });
